f6dd45cdd88268b8132db92848e0734d529bd194
risalstr <risa.lestari2002@gmail.com>
2023-09-15 12:16:11 +0700

    update incubation

--- incubation/urls.py
+++ incubation/urls.py
@@ -1,8 +1,26 @@
 from django.urls import path
-from .views import IncubationListAPIView, PortfolioAPIView, InvestorAPIView
+from .views import (
+    IncubationListAPIView,
+    PortfolioAPIView,
+    InvestorAPIView,
+    IncubationDetailAPIView,
+)
 
 urlpatterns = [
-    path('incubation/', IncubationListAPIView.as_view(), name='incubation-list'),
-    path('incubation/investor/<int:id>/', InvestorAPIView.as_view(), name='incubation-list'),
-    path('incubation/portfolio/<int:portfolio_id>/', PortfolioAPIView.as_view(), name='incubation-list'),
+    path("incubation/", IncubationListAPIView.as_view(), name="incubation-list"),
+    path(
+        "incubation/<int:id>/",
+        IncubationDetailAPIView.as_view(),
+        name="incubation-list",
+    ),
+    path(
+        "incubation/investor/<int:id>/",
+        InvestorAPIView.as_view(),
+        name="incubation-list",
+    ),
+    path(
+        "incubation/portfolio/<int:portfolio_id>/",
+        PortfolioAPIView.as_view(),
+        name="incubation-list",
+    ),
 ]
--- incubation/views.py
+++ incubation/views.py
@@ -2,23 +2,34 @@ from rest_framework import generics
 from .models import Incubation, Portfolio, Investor
 from .serializers import IncubationSerializer, PortfolioSerializer, InvestorSerializer
 
+
 class IncubationListAPIView(generics.ListAPIView):
     queryset = Incubation.objects.all()
     serializer_class = IncubationSerializer
+
+
+class IncubationDetailAPIView(generics.ListAPIView):
+    serializer_class = IncubationSerializer
+
+    def get_queryset(self):
+        incubation_id = self.kwargs["id"]
+
+        return Incubation.objects.filter(id=incubation_id)
+
 
 class PortfolioAPIView(generics.ListAPIView):
     serializer_class = PortfolioSerializer
 
     def get_queryset(self):
-        portfolio_id = self.kwargs['portfolio_id']
-        
+        portfolio_id = self.kwargs["portfolio_id"]
+
         return Portfolio.objects.filter(id=portfolio_id)
-    
+
+
 class InvestorAPIView(generics.ListAPIView):
     serializer_class = InvestorSerializer
 
     def get_queryset(self):
-        investor_id = self.kwargs['id']
-        
+        investor_id = self.kwargs["id"]
+
         return Investor.objects.filter(id=investor_id)
-    
